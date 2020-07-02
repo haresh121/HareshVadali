6b943b6d19b72475f3a65dfaab98cfdd06b33f73
HARESH <34397532+haresh121@users.noreply.github.com>
2020-07-02 13:44:57 +0530

    Initial WebPage commit

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-// window.onload = (function(){
-//     setTimeout(function(){
-//         // $('.start-animation').animate({opacity: "0"}, 1000, function () {
-//             window.scrollTo(0, 0);
-//             $('.start-animation').css("display", "none");
-//             $("html").removeClass("toggle-overflow");
-//             $("body").removeClass("toggle-overflow");
-//                 // });
-//     }, 4000);
-// });
-// var tl = gsap.timeline({defaults: { duration: 1, stagger: 0.4}});
-// tl.from('.nav-link', {y: -20, duration:1.5, delay:4, stagger: 0.3, opacity:0})
-//     .from('.header-h1', {y: 20, opacity: 0, stagger: 0.3, delay: -2})
-//     .from('.sides-nav', {y:60, opacity:0, delay: 0.1});
+window.onload = (function(){
+    setTimeout(function(){
+        // $('.start-animation').animate({opacity: "0"}, 1000, function () {
+            window.scrollTo(0, 0);
+            $('.start-animation').css("display", "none");
+            $("html").removeClass("toggle-overflow");
+            $("body").removeClass("toggle-overflow");
+                // });
+    }, 4000);
+});
+var tl = gsap.timeline({defaults: { duration: 1, stagger: 0.4}});
+tl.from('.nav-link', {y: -20, duration:1.5, delay:4, stagger: 0.3, opacity:0})
+    .from('.header-h1', {y: 20, opacity: 0, stagger: 0.3, delay: -2})
+    .from('.sides-nav', {y:60, opacity:0, delay: 0.1});
 
 
 $(function() {
